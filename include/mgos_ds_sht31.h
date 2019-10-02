95466762bd47500a07428073d36d5d1bfe4879b3
Namo Aton <namoaton@gmail.com>
2019-10-02 16:29:39 +0300

    t

--- include/mgos_ds_sht31.h
+++ include/mgos_ds_sht31.h
@@ -21,7 +21,8 @@ typedef struct SHT31Tag SHT31;
 
     //SHT31 *mgos_ds_sht31_create(DS28E17Rmt *ds, char *ds_addr, uint8_t addr);
     SHT31 *mgos_ds_sht31_create(DS28E17Rmt * ds, char* ds_addr,  uint8_t addr)  {
-        if (ds == nullptr) return nullptr;
+       // if (ds == nullptr) return nullptr;
+        return  NULL;//SHT31(ds,ds_addr,addr);
         return new SHT31(ds,ds_addr,addr);
     };
 
